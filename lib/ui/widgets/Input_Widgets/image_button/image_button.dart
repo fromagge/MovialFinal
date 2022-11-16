cf201c5037307502116fefea240f66c06645afd5
Julio Mendoza Berdugo <jberdugoce@gmail.com>
2022-11-16 14:24:19 -0500

    arreglado el onTap del image_button

--- lib/ui/widgets/Input_Widgets/image_button/image_button.dart
+++ lib/ui/widgets/Input_Widgets/image_button/image_button.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:oferi/ui/pages/main/home/result_page.dart';
 
 class ImageButton extends StatelessWidget {
   final String text;
   final String imgPath;
   final Color color;
+  final double radius;
   const ImageButton(
       {super.key,
       required this.text,
       required this.imgPath,
-      this.color = const Color(0xFF42006E)});
+      this.color = const Color(0xFF42006E),
+      this.radius = 50});
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +20,20 @@ class ImageButton extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Ink(
-              decoration: const ShapeDecoration(
-                shape: CircleBorder(),
-              ),
-              child: CircleAvatar(
-                backgroundColor: color,
-                radius: 50.0,
-                child: Container(
-                    padding: const EdgeInsets.all(15),
-                    child: Image(image: AssetImage(imgPath))),
-              )),
+          InkWell(
+            borderRadius: BorderRadius.circular(radius - 7),
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: radius,
+              child: Container(
+                  padding: const EdgeInsets.all(15),
+                  child: Image(image: AssetImage(imgPath))),
+            ),
+            onTap: () {
+              //Filtrar por categoria en controlador
+              Get.to(Result(search: text));
+            },
+          ),
           Text(
             text,
             style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
