b63c3199d6cfdb3e96b1b4a9603627985b547d64
Gabriel García <ygabo@pm.me>
2022-11-26 19:52:42 -0500

    added favorites controller

--- lib/domain/entities/cart.dart
+++ lib/domain/entities/cart.dart
@@ -7,12 +7,14 @@ class Cart {
   Cart({required this.id, required this.items});
 
   factory Cart.fromJson(String id, Map<String, dynamic> json) {
-    print("items are $json");
-    print(json["items"]);
     return Cart(id: id, items: json["items"]);
   }
 
   void addElementToCart(String productId) {
     items.add(productId);
+  }
+
+  void eraseElementFromCart(String productId) {
+    items.remove(productId);
   }
 }
