46da4757689f71a510ff7e57071c31869566d238
Gabriel García <ygabo@pm.me>
2022-11-27 23:20:32 -0500

    added file controller

--- lib/ui/controllers/file_controller.dart
+++ lib/ui/controllers/file_controller.dart
@@ -6,9 +6,7 @@ Future<String> uploadFileToServer(File f) async {
   String imageId = nanoid(64);
   final storageRef = FirebaseStorage.instance.ref().child('public/$imageId');
 
-  UploadTask response =  storageRef.putFile(f);
+  await storageRef.putFile(f);
 
-  response.whenComplete(() => ).
-
-  return 'ERROR!';
+  return await storageRef.getDownloadURL();
 }
